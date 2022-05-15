df3b4f00ff0d25b34fc5fec5b197e1f9021d8131
amittgi123 <amit.tgi@gmail.com>
2022-05-15 11:44:48 +0530

    project

---
+++ AutomationGitConcept/src/CICDpipeline.java
@@ -0,0 +1,10 @@
+
+public class CICDpipeline {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Learing CI / CD program file for data repo");
+	}
+
+}
